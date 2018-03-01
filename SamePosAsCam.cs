c5e6d9ecf6a097887c2955bb0a72bb3f68f4352d
Natalie Le Huenen <32916975+natalielh@users.noreply.github.com>
2018-03-01 12:31:28 -0500

    added SamePosAsCam.cs

---
+++ SamePosAsCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamePosAsCam : MonoBehaviour {
+
+	Vector3 campos = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		campos = Camera.main.transform.position;
+
+		//set the current transform's x and z values to the same values as the main camera
+		//by default, the y-position is kept at ZERO every frame
+		transform.position = new Vector3(campos.x, 0, campos.z);
+	}
+}
